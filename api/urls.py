50e756ab3ef597a59fe91866503cd38157587c84
_pedy_d_ <pedy40000@gmail.com>
2022-11-06 02:59:12 +0330

    fix urls

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('app', views.AppViewSet.as_view({'get': 'list', 'post': 'create', 'delete': 'delete'})),
-    path('app/<slug:id>', views.AppViewSet.as_view({'get': 'get', 'post': 'edit'})),
+    path('app', views.AppViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('app/<slug:id>', views.AppViewSet.as_view({'get': 'get', 'post': 'edit', 'delete': 'delete'})),
     path('app/history', views.AppViewSet.as_view({'get': 'actionsHistory'})),
     path('app/<slug:id>/run', views.AppViewSet.as_view({'get': 'run'})),
 ]
