df489e4010c7c53032884eab31e56bc289428e4d
_pedy_d_ <pedy40000@gmail.com>
2022-11-06 02:59:17 +0330

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class App(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.CharField(max_length=256)
+    envs = models.CharField(max_length=256)
+    command = models.CharField(max_length=256)
