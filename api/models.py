54e490409f1bd630d52226ebea0b35229b8320d0
_pedy_d_ <pedy40000@gmail.com>
2022-11-06 15:08:45 +0330

    write and test run endpoint

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class App(models.Model):
-    name = models.CharField(max_length=256, unique=True)
+    name = models.CharField(max_length=256)
     image = models.CharField(max_length=256)
     environment = models.CharField(max_length=256, default='{}')
     command = models.CharField(max_length=256)
